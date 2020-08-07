6d0e29de60054e029571d87d8625ad194541948a
zsj <lekyzsj@gmail.com>
2020-08-08 00:54:31 +0800

    fix typo

--- ext/opencc/opencc.h
+++ ext/opencc/opencc.h
@@ -5,7 +5,7 @@
 #include <opencc/opencc.h>
 
 static VALUE rb_opencc_open(VALUE self, VALUE rb_cfg);
-static VALUE rb_opencc_convert_utf8(VALUE self, VALUE rb_opencc, VALUE rb_ocid);
+static VALUE rb_opencc_convert_utf8(VALUE self, VALUE rb_ocid, VALUE rb_str);
 static VALUE rb_opencc_close(VALUE self, VALUE rb_ocid);
 
 #endif /* RUBY_OPENCC_H */
