57c099acb03adf25e17304726cdc66ca0e070507
zsj <lekyzsj@gmail.com>
2020-08-25 13:08:12 +0800

    travis compiler

--- .travis/install.sh
+++ .travis/install.sh
@@ -3,5 +3,10 @@
 if [ $TRAVIS_OS_NAME = 'osx' ]; then
 	brew install opencc
 else
-	sudo apt install -y libopencc-dev
+  cd /usr/src
+  git clone https://github.com/BYVoid/OpenCC
+  cd OpenCC
+  git checkout ver.1.1.1
+  make PREFIX=/usr/local
+  sudo make install
 fi
